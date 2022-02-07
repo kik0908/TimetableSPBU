6c4edfd4f8e83922468eb627cf692920c8770d5e
kik0908 <kik0908@yandex.ru>
2022-02-07 11:06:23 +0300

    Pushing async version

--- spbuTimetable/__init__.py
+++ spbuTimetable/__init__.py
@@ -1,20 +1,3 @@
 import unittest
 
 from spbuTimetable.parser import get_groups, get_programs, get_lessons, get_fields_of_study
-
-
-class ParserTest(unittest.TestCase):
-    def test_get_fields_of_study(self):
-        pass
-
-    def test_get_programs_1(self):
-        pass
-
-    def test_get_programs_2(self):
-        pass
-
-    def test_get_programs_3(self):
-        pass
-
-    def test_get_programs_4(self):
-        pass
---
+++ spbuTimetable/async_parser/__init__.py
@@ -0,0 +1,1 @@
+from spbuTimetable.async_parser.parser import as_get_fields_of_study, as_get_groups, as_get_programs, as_get_lessons
